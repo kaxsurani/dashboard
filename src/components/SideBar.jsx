a5a421a576839b4b39daf2675ca982942dcb0aaa
Satish <stshkakdiya@gmail.com>
2024-09-30 17:40:11 +0530

    sidebar change

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,59 +1,126 @@
-import { useState } from "react";
-import Arrow from "../Assets/arrowup.jpg";
+// import { useState } from "react";
+// import Arrow from "../Assets/arrowup.jpg";
+// import data from "./data";
+// import { CgMenuLeft } from "react-icons/cg";
+// import { IoCloseSharp } from "react-icons/io5";
+// const SideBar = () => {
+//   const [isOpen, setIsOpen] = useState(true);
+
+//   return (
+//       <div
+//         className={`top-0 h-full bg-[#3f4d67] shadow-lg transition-transform transform absolute w-[67vw] md:w-[16%] ${
+//           isOpen ? "translate-x-0" : "-translate-x-full"
+//         } ${isOpen && "md:sticky"} h-[100vh] pl-[1%] pt-[20px]`}
+//       >
+//         <>
+//           <div className={`flex justify-between text-[#a9b7d0] text-[22px] pr-4 cursor-pointer`}>
+//             <div className="flex gap-3 mb-9 cursor-pointer items-center">
+//               <img className="size-10 rounded-xl" src={Arrow} alt="" />
+//               <p className="text-[18px] text-[#fff]">Datta Able</p>
+//             </div>
+//            {!isOpen && <CgMenuLeft
+//               onClick={() => {
+//                 setIsOpen(true);
+//               }}
+//             />}
+//            {isOpen && <IoCloseSharp  onClick={() => {
+//                 setIsOpen(false);
+//               }}/>}
+//           </div>
+//           {data.map((user) => (
+//             <div
+//               key={user.id}
+//               className="grid grid-cols-1 gap-4 cursor-pointer items-center"
+//             >
+//               <div className="flex flex-col gap-4">
+//                 <div className="text-[10px] font-bold text-[#e8edf7]">
+//                   {user.name}
+//                 </div>
+//                 <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
+//                   {user.icon}
+//                   <p>{user.name1}</p>
+//                 </div>
+//               </div>
+//               <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
+//                 {user.icon1}
+//                 <p>{user.name2}</p>
+//               </div>
+//               <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
+//                 {user.icon2}
+//                 <p>{user.name3}</p>
+//               </div>
+//               <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
+//                 {user.icon3}
+//                 <p>{user.name4}</p>
+//               </div>
+//             </div>
+//           ))}
+//         </>
+//       </div>
+//   );
+// };
+
+// export default SideBar;
+import React, { useState } from "react";
 import data from "./data";
 import { CgMenuLeft } from "react-icons/cg";
-const SideBar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+import { IoCloseSharp } from "react-icons/io5";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-      <div
-        className={`top-0 h-full bg-[#3f4d67] w-[16%] shadow-lg transition-transform transform ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        } ${isOpen ? "sticky" : "fixed"} h-[100vh] pl-[1%] pt-[20px]`}
+    <div
+      className={`${
+        isOpen ? "absolute" : ""
+      } top-0 h-full bg-[#3f4d67] shadow-lg transition-width duration-300 ${
+        isOpen && "md:sticky"
+      } ${isOpen ? "w-64" : "w-13"}
+      ${
+        !isOpen && "sticky"
+      }`}
+    >
+      <button
+        type="button"
+        onClick={toggleSidebar}
+        className="flex items-center justify-center p-2 text-[#a9b7d0]"
       >
-        <div className="">
-          <div className="flex justify-between text-[#a9b7d0] text-[22px] pr-4 cursor-pointer">
-            <div className="flex gap-3 mb-9 cursor-pointer items-center">
-              <img className="size-10 rounded-xl" src={Arrow} alt="" />
-              <p className="text-[18px] text-[#fff]">Datta Able</p>
+        {isOpen ? <IoCloseSharp size={24} /> : <CgMenuLeft size={24} />}
+      </button>
+      <nav>
+        {data.map((item, index) => (
+          <div key={index} className="flex flex-col gap-4">
+            <div className="flex items-center p-2 text-[#a9b7d0]">
+              {isOpen && <span className="ml-2 text-[#e8edf7] text-[10px] font-semibold">{item.name}</span>}
             </div>
-            <CgMenuLeft
-              onClick={() => {
-                setIsOpen(!isOpen);
-              }}
-            />
+            {item.subNav && (
+              <div className={`${isOpen ? "pl-4" : "0px"}`}>
+                {item.subNav.map((subItem, subIndex) => (
+                  <div
+                    key={subIndex}
+                    className={`flex items-center text-[#a9b7d0] text-[14px] ${
+                      isOpen ? "" : "justify-center"
+                    } pb-5 p-2 text-white hover:text-[#1dc4e9]`}
+                  >
+                    {subItem.icon}
+                    {isOpen && (
+                      <span className="ml-2 text-[#a9b7d0] hover:text-[#1dc4e9]">
+                        {subItem.name}
+                      </span>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-          {data.map((user) => (
-            <div
-              key={user.id}
-              className="grid grid-cols-1 gap-4 cursor-pointer items-center"
-            >
-              <div className="flex flex-col gap-4">
-                <div className="text-[10px] font-bold text-[#e8edf7]">
-                  {user.name}
-                </div>
-                <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
-                  {user.icon}
-                  <p>{user.name1}</p>
-                </div>
-              </div>
-              <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
-                {user.icon1}
-                <p>{user.name2}</p>
-              </div>
-              <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
-                {user.icon2}
-                <p>{user.name3}</p>
-              </div>
-              <div className="flex items-center gap-3 text-[#a9b7d0] hover:text-[#1dc4e9] pl-[2px]">
-                {user.icon3}
-                <p>{user.name4}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+        ))}
+      </nav>
+    </div>
   );
 };
 
-export default SideBar;
+export default Sidebar;
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -10,35 +10,78 @@ import { FaRegQuestionCircle } from "react-icons/fa";
 import { MdMenu } from "react-icons/md";
 
 const data = [
-    { id: 1, name: "NAVIGATION", icon: <LuHome size={18} />, name1: "Dashboard" },
-    { id: 2, name: "UI ELEMENT", icon: <FiBox size={18} />, name1: "Component" },
-    {
-      id: 3,
-      name: "Forms & Tables",
-      icon: <SiGoogleforms size={18} />,
-      name1: "Form Elements",
-      icon1: <MdOutlineBackupTable size={18} />,
-      name2: "Table",
-    },
-    {
-      id: 4,
-      name: "Chart & Maps",
-      icon: <FaChartPie size={18} />,
-      name1: "Charts",
-      icon1: <FaRegMap size={18} />,
-      name2: "Map",
-    },
-    {
-      id: 5,
-      name: "Pages",
-      icon: <IoLockClosedOutline size={18} />,
-      name1: "Authentication",
-      icon1: <MdOutlineFindInPage size={18} />,
-      name2: "Sample Page",
-      icon2: <FaRegQuestionCircle size={18} />,
-      name3: "Documentation",
-      icon3: <MdMenu size={18} />,
-      name4: "Menu Levels"
-    },
-  ];
+  { 
+    name: "NAVIGATION",
+    subNav: [
+      {icon: <LuHome size={18} />,
+    name: "Dashboard", 
+    path: "/dashboard" },
+    ],
+  },
+  {
+    name: "UI ELEMENT",
+    subNav: [
+      {  name: "NAVIGATION",
+        icon: <FiBox size={18} />,
+        name: "Component", 
+        path: "" },
+    ],
+  },
+  {
+    name: "Forms & Tables",
+    subNav: [
+      { name: "Form Elements", icon: <SiGoogleforms size={18} />, path: "" },
+      { name: "Table", icon:<MdOutlineBackupTable size={18} />, path: "" },
+    ],
+  },
+  {
+    name: "Chart & Maps",
+    subNav: [
+      { name: "Charts", icon: <FaChartPie size={18} />, path: "" },
+      { name: "Map", icon: <FaRegMap size={18} />, path: "" },
+    ],
+  },
+  {
+    name: "Pages",
+    subNav: [
+      { name: "Authentication", icon: <IoLockClosedOutline size={18} />, path: "" },
+      { name: "Sample Page", icon: <MdOutlineFindInPage size={18} />, path: "" },
+      { name: "Documentation", icon: <FaRegQuestionCircle size={18} />, path: "" },
+      { name: "Menu Levels", icon: <MdMenu size={18} />, path: "" },
+    ],
+  },
+];
+
+// const data = [
+    // { id: 1, name: "NAVIGATION", icon: <LuHome size={18} />, name1: "Dashboard" },
+    // { id: 2, name: "UI ELEMENT", icon: <FiBox size={18} />, name1: "Component" },
+    // {
+    //   id: 3,
+    //   name: "Forms & Tables",
+    //   icon: <SiGoogleforms size={18} />,
+    //   name1: "Form Elements",
+    //   icon1: <MdOutlineBackupTable size={18} />,
+    //   name2: "Table",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Chart & Maps",
+    //   icon: <FaChartPie size={18} />,
+    //   name1: "Charts",
+    //   icon1: <FaRegMap size={18} />,
+    //   name2: "Map",
+    // },
+    // {
+    //   id: 5,
+    //   name: "Pages",
+    //   icon: <IoLockClosedOutline size={18} />,
+    //   name1: "Authentication",
+    //   icon1: <MdOutlineFindInPage size={18} />,
+    //   name2: "Sample Page",
+    //   icon2: <FaRegQuestionCircle size={18} />,
+    //   name3: "Documentation",
+    //   icon3: <MdMenu size={18} />,
+    //   name4: "Menu Levels"
+    // },
+  // ];
   export default data;
